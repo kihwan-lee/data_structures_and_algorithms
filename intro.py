f2e3d3c230f52a26a2d1b354f1087563f2219b30
kihwan-lee <kihwanlee97@gmail.com>
2021-03-08 22:14:52 -0600

    1.3.2 notes

--- intro.py
+++ intro.py
@@ -3,9 +3,47 @@
 #1.3 ----------------- WHAT IS COMPUTER SCIENCE?
 
 '''
-Computer science is the study of problems, problem-solving, and the solutions that come out of the problem-solving process. Given a problem, a computer scientist’s goal is to develop an algorithm, a step-by-step list of instructions for solving any instance of the problem that might arise. Algorithms are finite processes that if followed will solve the problem. Algorithms are solutions.
-
+Computer science is the study of problems, problem-solving, and the solutions that come out of the problem-solving process. 
 
 We say that a problem is computable if an algorithm exists for solving it.
-'''
 
+Algorithm:
+- Step by step list of instructions for solving and unstrance of the problem that might arise
+- Finite processes taht if followed will solve the problem
+- Algorithms are solutions
+
+Abstraction:
+- Allows us to view the problem and solution in such a way as to separate the so-called logical and physical perspectives
+- Logical Perspective: Interface, The User
+- Physical Perspective: "Under the Hood", The Programmer
+
+- Procedural Abstraction:
+  - We do not necessarily know how an operation is being performed but we know what the function is called and how to use it.
+  - We describe the interface: the name of the funciton, what is needed(the parameters), and what will be returned. The details of the actual operation is hidden inside. 
+'''
+#1.3.1 ----------------- WHAT IS PROGRAMMING?
+
+'''
+Programming:
+- Process of taking an algorithm and executing it by a computer
+- Without an algorithm there can be no program
+
+Data Types:
+- Provide an interpretation for binary data so that we can think about the data in terms that make sense
+- Primitive data types provide the building blocks for algorithm development
+'''
+#1.3.2 ----------------- WHY STUDY DATA STRUCTURES & ABSTRACT DATA TYPES?
+
+'''
+To manage the complexity of problems and the problem-solving process, computer scientists use abstractions to allow them to focus on the “big picture” without getting lost in the details.
+
+Information Hiding: 
+- By encapsulating the details of the implementation, we are hiding them from the user’s view
+
+How an abstract data type operates:
+- The user interacts with the interface, using the operations that have been specified by the abstract data type
+- The abstract data type is the shell that the user interacts with
+- The implementation is hidden one level deeper
+- The user is not concerned with the details of the implementation
+
+'''
