eab3cd312a08953dcca311665820827fa1290bec
kihwan-lee <kihwanlee97@gmail.com>
2021-03-08 21:47:34 -0600

    1.2, 1.3 Notes

---
+++ intro.py
@@ -0,0 +1,11 @@
+#1.2 ----------------- GETTING STARTED 
+
+#1.3 ----------------- WHAT IS COMPUTER SCIENCE?
+
+'''
+Computer science is the study of problems, problem-solving, and the solutions that come out of the problem-solving process. Given a problem, a computer scientist’s goal is to develop an algorithm, a step-by-step list of instructions for solving any instance of the problem that might arise. Algorithms are finite processes that if followed will solve the problem. Algorithms are solutions.
+
+
+We say that a problem is computable if an algorithm exists for solving it.
+'''
+
