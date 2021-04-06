2d9519a155c3e496312d50876ba2e8fc5ba1b42d
kihwan-lee <kihwanlee97@gmail.com>
2021-04-06 00:16:13 -0500

    Organizing my notes

--- basic_data_structures/basic_data_structures.py
+++ basic_data_structures/basic_data_structures.py
@@ -44,27 +44,3 @@ Linear Data Structures:
   Some structures might only allow new items to be added at only one end.
   Some structures might allow items to be removed from either end. Just depends. 
 '''
-# 4.3 ----------------- WHAT IS A STACK?
-
-'''
-Stack:
-- An ordered collection of items where the addition of new items and the removal of existing items always takes place at the same end.
-  This end is commonly referred to as the "top".
-  The end opposite of the top is known as the "base".
-  Example: 
-  Think of a stack of books.
-
-- The base of the stack is significant since items stored in the stack that are closer to the base represent those that have beeen in 
-the stack the longest.
-
-- LIFO: Last-in first-out
-  - The most recently added item is the one that is in position to be removed first.
-  - This principle provides an ordering based on length of time in the collection. 
-  - Newer items are near the top, while older items are near the base. 
-
-Strength of Stacks:
-- GREAT for reversing the order of items. 
-- Keeping track of state or when things have occured. 
-'''
-
-
