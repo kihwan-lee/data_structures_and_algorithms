950af5774c11576f99d5f06fe39575cf43d6c3d4
kihwan-lee <kihwanlee97@gmail.com>
2021-04-07 17:08:49 -0500

    Balanced Parentheses

--- basic_data_structures/stacks.py
+++ basic_data_structures/stacks.py
@@ -1,3 +1,4 @@
+from pythonds.basic import Stack
 # 4.3 ----------------- WHAT IS A STACK?
 
 '''
@@ -22,4 +23,79 @@ Strength of Stacks:
 - Keeping track of state or when things have occured. 
 '''
 
+# 4.4 ----------------- THE STACK ABSTRACT DATA TYPE
 
+'''
+Stack() : Creates a new stack that is empty. It needs no parameters and returns an empty stack.
+
+push(item) : Adds a new item to the top of the stack. It needs the item and returns nothing.
+
+pop() : Removes the top item from the stack. It needs no parameters and returns the item. The stack is modified.
+
+peek() : Returns the top item from the stack but does not remove it. It needs no parameters. The stack is not modified.
+
+isEmpty() : Tests to see whether the stack is empty. It needs no parameters and returns a boolean value.
+
+size() : Returns the number of items on the stack. It needs no parameters and returns an integer.
+'''
+
+# 4.5 ----------------- IMPLEMENTATING A STACK IN PYTHON
+
+class Stack:
+  def __init__(self):
+    self.items = []
+
+  def isEmpty(self):
+    return self.items == []
+
+  def push(self, item):
+    self.items.append(item)
+
+  def pop(self):
+    return self.items.pop()
+
+  def peek(self):
+    return self.items[len(self.items)-1]
+
+  def size(self):
+    return len(self.items)
+
+
+def reverse_string(mystr):
+  s = list(mystr)
+  n = []
+  print(s)
+  while len(s) > 0:
+    item = s.pop()
+    n.append(item)
+
+  print(n)
+
+reverse_string('apple')
+
+# Didn't really implement it but it doesn't seem too important because I just learn how to use a list effectively.
+
+# 4.6 ----------------- SIMPLE BALANCED PARENTHESES
+
+'''
+Balanced Parentheses:
+- Means that each opening symbol has a corresponding closing symbol and the pairs of parentheses are properly nested.
+
+Balanced Example:
+(()()()())
+(((())))
+(()((())()))
+
+Not Balanced Example:
+(((((())
+()))
+(()()(()
+'''
+
+'''
+Challenge:
+  - Write an algorithm that will read a string of parentheses from left to right and decide whether the symbols are balanced. 
+    - As you process symbols from left to right, the most recent opening parenthesis must match the next closing symbol. 
+    - Also, the first opening symbol processed may have to wait until the very last symbol for its match. 
+    - Closing symbols match opening symbols in the reverse order of their appearance; they match from the inside out. 
+'''
