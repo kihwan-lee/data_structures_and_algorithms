32ea362f1b70ff7bd94cc2352fa819f49a79367b
kihwan-lee <kihwanlee97@gmail.com>
2021-04-11 23:15:28 -0500

    Starting queues

---
+++ basic_data_structures/queues.py
@@ -0,0 +1,20 @@
+# 4.10 ----------------- WHAT IS A QUEUE?
+
+'''
+Explanation:
+  - A queue is an ordered collection of items where the addition of new items happens at one end, called the "rear".
+  - The removal of existing items occurs at the other end, commonly called the "front".
+  - As an element enters the queue it starts at the rear and makes its way toward the front, waiting until that time when it is the next element to be removed.
+  - There is no jumping in the middle and no leaving before you have waited the necessary amount of time to get to the front.
+
+
+FIFO: First In, First Out
+  - The most recently added item in the queue must wait at the end of the collection. 
+  - The item that has been in the collection the longest is at the front. 
+  - Also known as "first come, first served".
+
+Examples:
+  - Waiting in line for a movie, check-out line at a grocery store, waiting in a cafeteria line
+'''
+
+# 4.11 ----------------- THE QUEUE ABSTRACT DATA TYPE
