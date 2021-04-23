da24d54e8cb39f1e8359d2eb6ad48a1835556531
kihwan-lee <kihwanlee97@gmail.com>
2021-04-22 21:15:00 -0500

    Starting Deques

---
+++ basic_data_structures/deques.py
@@ -0,0 +1,10 @@
+# 4.15 ----------------- WHAT IS A DEQUE?
+
+'''
+Deque:
+- Also known as a double-ended queue, is an ordered collection of items similar to the queue. 
+- It has two ends, a front an a rear, and the items remain positioned in the collection. 
+- Main difference is the unrestrictive nature of adding and removing items.
+  - New items can be added at either the front of the rear. 
+  
+'''
